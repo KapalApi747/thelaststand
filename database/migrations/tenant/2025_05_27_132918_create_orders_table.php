cab1290d3928876f23e6d02c78dd7ea46d05e48c
Yuri Steijaert <yuri.nethsienanda@gmail.com>
2025-06-20 23:42:47 +0200

    bugfixes

--- database/migrations/tenant/2025_05_27_132918_create_orders_table.php
+++ database/migrations/tenant/2025_05_27_132918_create_orders_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('customer_id')->constrained('customers')->cascadeOnDelete();
             $table->string('order_number')->unique();
             $table->string('session_id')->nullable()->unique();
-            $table->string('payout_id')->nullable()->unique();
+            $table->string('payout_id')->nullable();
             $table->decimal('total_amount', 10, 2);
             $table->decimal('tax_amount', 10, 2)->nullable();
             $table->decimal('shipping_cost', 10, 2)->nullable();
